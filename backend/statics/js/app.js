3b4898bcf7cdb0778f8eb146e7742a9aed6593a4
Takumi Kotani <takumi59267@gmail.com>
2025-11-30 21:00:58 +0900

    ドキュメントの追加

--- backend/statics/js/app.js
+++ backend/statics/js/app.js
@@ -265,7 +265,7 @@
     try{
       //常に同じURLへPOSTする
 
->>>>>>> 7cf47ebbdf95d3a60abad83fa0dda3b728bf055e
+      // 7cf47ebbdf95d3a60abad83fa0dda3b728bf055e
       const response = await fetch("http://localhost:8000/api/review_sessions",{
         method: "POST",
         //このリクエストの本文はJSON形式であることを明記
